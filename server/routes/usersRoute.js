346f6b679a10bb780c3a82774556062142050c72
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-25 15:00:37 +0530

    user model and route

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const User = require("../models/userModel");
+
+// register user account
+
+router.post("/register", async (req, res) => {
+  try {
+    // check if user already exists
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.send({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send({
+      message: "User created successfully",
+      data: null,
+      success: true,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 require("dotenv").config();
 app.use(express.json());
 const dbConfig = require("./config/dbConfig");
+const usersRoute = require("./routes/usersRoute");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
